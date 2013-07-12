623746d71434e8e917756729429d5ae8361ec219
Joseph Krump <krump.joseph2gmail.com>
2013-07-12 03:29:09 +0100

    added settings.php to be tracked

---
+++ settings.php
@@ -0,0 +1,58 @@
+<?php 
+	include 'includes/core/init.php';
+	protect_page();
+	include_once('includes/overall/overall_header.php'); 
+
+	if(!empty($_POST) && (($_POST['f_name'] != $user_data['f_name']) || ($_POST['l_name'] != $user_data['l_name']) || ($_POST['email'] != $user_data['email']))){
+
+		if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+			$errors[] = 'A valid email address is required';
+		}
+		if(email_exists($_POST['email']) && ($_POST['email'] != $user_data['email'])){
+			$errors[] = 'This email address is already in use';
+		}
+	}
+?>
+<h1>Settings</h1> 
+<?php 
+if(isset($_GET['success'])){
+	echo 'Your details have been updated!';
+} else {
+	if(!empty($_POST) && empty($errors)){
+		$update_data = array(
+			'f_name' => $_POST['f_name'],
+			'l_name' => $_POST['l_name'],
+			'email' => $_POST['email']
+		);
+
+		update_data($update_data);
+		header('Location: settings.php?success');
+		exit();
+
+	} else if (!empty($errors)){
+		output_errors($errors);
+	}
+
+	?>
+	<form action="" method="post">
+		<ul>
+			<li>
+				First name:<br/>
+				<input type="text" name="f_name" value="<?php echo $user_data['f_name']; ?>" autofocus/>
+			</li>
+			<li>
+				Last name:<br />
+				<input type="text" name="l_name" value="<?php echo $user_data['l_name']; ?>" autofocus/>
+			</li>
+			<li>
+				Email:<br />
+				<input type="text" name="email" value="<?php echo $user_data['email']; ?>" autofocus/>
+			<li>
+				<input type="submit" value="Update"/>
+			</li>
+		</ul>
+	</form>
+<?php 
+}
+include_once('includes/overall/overall_footer.php'); 
+?>
